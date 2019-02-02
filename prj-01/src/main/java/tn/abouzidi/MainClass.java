52274445203e2d8414b0e6dd202bf72a95471d1a
AfifBouzidi <afif.bouzidi@gmail.com>
2019-02-02 23:44:26 +0100

    Création par héritage de la classe Thread

---
+++ prj-01/src/main/java/tn/abouzidi/MainClass.java
@@ -0,0 +1,10 @@
+package tn.abouzidi;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+       CustomThread customThread =new CustomThread();
+       customThread.start();
+	}
+
+}
